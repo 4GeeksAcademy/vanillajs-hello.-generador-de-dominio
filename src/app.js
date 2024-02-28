39801ceb31fc6e84178df9b850b1eb00e9f3a7ca
CesarMendez1 <152318003+CesarMendez1@users.noreply.github.com>
2024-02-28 00:02:29 +0000

    completada la actividad. reesultado de muest

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dominio = [".com", ".net", ".org", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noun.length; b++) {
+        for (let c = 0; c < dominio.length; c++) {
+          console.log(pronoun[i] + adj[a] + noun[b] + dominio[c]);
+        }
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
